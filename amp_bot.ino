b07e9cb563bddcfeb3ba82d8e6faa70191356695
Marc LaBelle <mjlabe@gmail.com>
2023-06-08 16:31:35 -0400

    remove setting switch pin and update comments

--- amp_bot.ino
+++ amp_bot.ino
@@ -4,11 +4,12 @@
 Knob* amp_knobs[];
 
 void setup() {
-  // put your setup code here, to run once:
   const int num_knobs = sizeof(knobs)/sizeof(int);
   for (int amp_knob = 0; amp_knob < num_knobs; amp_knob++) {
+    // setup global array of knob objects
     amp_knobs[amp_knob] = new Knob(knobs[i]._switch, knobs[i]._led, knobs[i]._knob);
-    attachInterrupt(digitalPinToInterrupt(knobs[i]._pin), button_pressed, CHANGE);  //  function for creating external interrupts at pin2 on Change
+    // Create external interrupt for pin on Change
+    attachInterrupt(digitalPinToInterrupt(knobs[i]._pin), button_pressed, CHANGE);
   }
 } 
 
@@ -22,8 +23,8 @@ class Knob(knob amp_knob) {
   float _knob_state_HIGH = amp_knob._knob_high;
 
   void switch() {
+    // change switch state and update LED
     _switch_state = (_switch_state == LOW)? HIGH: LOW;
-    digitalWrite(_switch, _switch_state);
     digitalWrite(_led, _switch_state);
 
     float _knob_state = (_knob_state == _knob_state_LOW)? _knob_state_HIGH: _knob_state_LOW;
@@ -43,6 +44,5 @@ void button_pressed() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
 }
