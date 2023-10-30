2cb04e55e6bf2d081839be57c45333d2901ff5c2
Marc LaBelle <mjlabe@gmail.com>
2023-10-30 09:12:54 -0400

    refactor settings and update readme

--- src/preset.py
+++ src/preset.py
@@ -1,14 +1,15 @@
 import ujson
 
+import settings
+
 from src.hardware.led import LED
 from src.hardware.servo import Servo
 from src.hardware.switch import Switch
-from src.settings import load_saved_settings
 
 
 class Preset(Switch):
     def __init__(self, switch_pin: int):
-        self.preset = self.get_preset()
+        self.preset = self.load_preset()
         super().__init__(switch_pin)
         self.switch = switch_pin
         self.led = LED(self.preset["led"])
@@ -27,17 +28,34 @@ class Preset(Switch):
             for servo in self.servos:
                 servo.set(self.preset)
 
-    def get_preset(self):
-        presets = load_saved_settings()
+    def load_preset(self):
+        presets = self.load_presets()
         return presets[self.switch]
 
     def save_preset(self):
         preset = {
             (servo.signal_pin, servo.feedback_pin): servo.get() for servo in self.servos
         }
-        presets = load_saved_settings()
+        presets = self.load_presets()
         presets[self.switch] = preset
         with open("settings.json", "wb") as settings_file:
             ujson.dump(presets, settings_file, indent=4)
         # flash to notify user preset is saved
         self.led.flash(num_times=5, frequency_ms=500)
+
+    @staticmethod
+    def load_presets():
+        # load settings as {SWITCH_PIN: {(SERVO_SGNL_PIN, SERVO_FDBK_PIN): SERVO_ANGLE}}
+        user_settings = {
+            i[0]: {
+                "led": i[1],
+                "servos": {j: 90 for j in settings.SERVO_PINS}
+            }
+            for i in settings.SWITCH_PINS
+        }
+        try:
+            with open("settings.json", "rb") as settings_file:
+                user_settings |= ujson.load(settings_file)
+        except Exception:
+            pass
+        return user_settings
--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,3 @@
-import ujson
-
 # set switch pins in ascending order
 SWITCH_PINS = [
     (16, 19,),
@@ -15,20 +13,3 @@ SERVO_PINS = [
     (12, 33,),
     (13, 25,),
 ]
-
-
-def load_saved_settings():
-    # load settings as {SWITCH_PIN: {(SERVO_SGNL_PIN, SERVO_FDBK_PIN): SERVO_ANGLE}}
-    user_settings = {
-        i[0]: {
-            "led": i[1],
-            "servos": {j: 90 for j in SERVO_PINS}
-        }
-        for i in SWITCH_PINS
-    }
-    try:
-        with open("settings.json", "rb") as settings_file:
-            user_settings |= ujson.load(settings_file)
-    except Exception:
-        pass
-    return user_settings
