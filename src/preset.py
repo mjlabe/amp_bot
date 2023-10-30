6e857ced87f33790565dbbf57a09e789a56992d9
Marc LaBelle <marc@Jennifers-MacBook-Air.local>
2023-10-29 20:50:07 -0400

    init and save presets

--- src/preset.py
+++ src/preset.py
@@ -6,13 +6,14 @@ from src.settings import load_saved_settings
 
 
 class Preset(Switch):
-    def __init__(self, switch_pin: int, servo_pins: [int], pin):
-        self.preset_values = self.get_preset_values()
-        super().__init__(pin)
+    def __init__(self, switch_pin: int):
+        self.preset = self.get_preset()
+        super().__init__(switch_pin)
         self.switch = switch_pin
         self.servos = []
-        for servo_pin in servo_pins:
-            servo = Servo(servo_pin)
+        for servo, _ in self.preset:
+            # get signal and feedback pins for all servos
+            servo = Servo(servo[0], servo[1])
             # set to noon on startup
             servo.set(90)
             self.servos.append(servo)
@@ -22,13 +23,15 @@ class Preset(Switch):
             self.save_preset()
         else:
             for servo in self.servos:
-                servo.set(self.preset_values)
+                servo.set(self.preset)
 
-    @staticmethod
-    def get_preset_values():
-        return load_saved_settings()
+    def get_preset(self):
+        presets = load_saved_settings()
+        return presets[self.switch]
 
     def save_preset(self):
-        settings = {"TO": "DO"}
+        preset = {(servo.signal_pin, servo.feedback_pin): servo.get() for servo in self.servos}
+        presets = load_saved_settings()
+        presets[self.switch] = preset
         with open("settings.json", "wb") as settings_file:
-            ujson.dump(settings, settings_file, indent=4)
+            ujson.dump(presets, settings_file, indent=4)
